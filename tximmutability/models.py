83a09aaeadaeadb3383d6eb388db89169d351ed6
Tomeu Pons <tomeu.pons@txerpa.com>
2021-08-10 16:56:33 +0200

    Refactor Rule & models
    Rule: Rename class , rename attrib, Model: refactor BaseImmutableModelUpdate

--- tximmutability/models.py
+++ tximmutability/models.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import logging
+from abc import ABCMeta
+from abc import abstractmethod
 
 from django.db import models
+from django.utils.translation import gettext as _
 from rest_framework.serializers import ValidationError
-from tximmutability.rule import ImmutabilityRule
-from abc import ABCMeta, abstractmethod, abstractproperty
+from tximmutability.rule import MutabilityRule
+logger = logging.getLogger('tximmutability')
 
 
 class BaseImmutableModelAction(object):
@@ -15,14 +21,20 @@ class BaseImmutableModelAction(object):
 
     To validate action against immutability rules call validate(rules) method  
     """
+    # output = _('Today is %(month)s %(day)s.') % {'month': m, 'day': d}
     __metaclass__ = ABCMeta
 
     def __init__(self, model_instance):
         self.model_instance = model_instance
 
-    @abstractmethod
+    def error_message(self, rule=None):
+        if isinstance(rule, MutabilityRule):
+            return rule.error_message % {'action': self.action}
+        return _('El item no se ha podido %(action).') % {'action': self.action}
+
+    @property
     def name(self):
-        pass
+        raise NotImplementedError
 
     @abstractmethod
     def is_allowed(self, rule):
@@ -40,26 +52,38 @@ class BaseImmutableModelAction(object):
         """
         for rule in immutability_rules:
             if not self.is_allowed(rule):
-                message = rule.error_message or f'No se puede {self.name} ítem'
-                raise ValidationError(message)
+                raise ValidationError(self.error_message(rule))
 
 
 class BaseImmutableModelCreate(BaseImmutableModelAction):
-    name = 'crear'
+    action = 'crear'
 
     def is_allowed(self, rule):
         """
         Create is allowed if rule byself allow creation or if model is in mutable state
         """
-        return rule.allow_create or rule.is_object_mutable(self.model_instance)
+        return rule.exclude_on_create or rule.is_mutable(self.model_instance)
 
 
 class BaseImmutableModelUpdate(BaseImmutableModelAction):
-    name = 'editar'
+    action = 'editar'
+    errors = {}
 
-    def __init__(self, model_instance, field_name):
+    def __init__(self, model_instance):
         super(BaseImmutableModelUpdate, self).__init__(model_instance)
-        self.field_name = field_name
+
+    def validate(self, immutability_rules):
+        """
+        :param: ImmutableModelAction []
+        :raise: ValidationError
+        """
+        for rule in immutability_rules:
+            self.is_allowed(rule)
+
+        if self.errors:
+            import inspect
+            stack = inspect.stack()
+            raise ValidationError(self.errors)
 
     def is_allowed(self, rule):
         """
@@ -71,18 +95,38 @@ class BaseImmutableModelUpdate(BaseImmutableModelAction):
         :param rule: ImmutabilityRule
         :return: bool
         """
-        return rule.allow_update or rule.field_name == self.field_name or rule.excluded_field(self.field_name) \
-            or rule.is_object_mutable(self.model_instance)
+
+        allowed = True
+        if rule.exclude_on_update:
+            return True
+
+        for field, value in self.model_instance.tracker.changed().items():
+            logger.info(_("Field updated"), field, value)
+
+            if field in rule.exclude_fields:
+                # excluded field
+                continue
+
+            if field == rule.field_name:
+                # same rule field
+                continue
+
+            # [tup[1] for tup in string.Formatter().parse(my_string) if tup[1] is not None]
+            if not rule.is_mutable(self.model_instance):
+                self.errors[field] = self.error_message(rule)
+                allowed = False
+
+        return allowed
 
 
 class BaseImmutableModelDelete(BaseImmutableModelAction):
-    name = 'borrar'
+    action = 'borrar'
 
     def is_allowed(self, rule):
         """
         Delete of the instance is allowed if rule by self allow delete or if instance is in mutable state
         """
-        return rule.allow_delete or rule.is_object_mutable(self.model_instance)
+        return rule.exclude_on_delete or rule.is_mutable(self.model_instance)
 
 
 class ImmutableModel(models.Model):
@@ -106,53 +150,63 @@ class ImmutableModel(models.Model):
 
     def check_immutability_rules(self):
         if not isinstance(self.immutability_rules, (tuple, list)):
-            raise TypeError('immutability_rules attribute in %s must be '
-                            'a list' % self.Meta.verbose_name)
-        if any(not isinstance(rule, ImmutabilityRule) for rule in self.immutability_rules):
-            raise TypeError('%s: immutability rule item has to be ImmutabilityRule type' % self.Meta.verbose_name)
+            raise TypeError(_('immutability_rules attribute in %s must be '
+                            'a list' % self.Meta.verbose_name))
+        if any(not isinstance(rule, MutabilityRule) for rule in self.immutability_rules):
+            raise TypeError(_('%s: immutability rule item has to be ImmutabilityRule type' % self.Meta.verbose_name))
 
     def save(self, *args, **kwargs):
-        if not self.pk:
-            BaseImmutableModelCreate(self).validate(self.immutability_rules)
+        tx_force = kwargs.pop("force", False)
+        if not tx_force:
+            if not self.pk:
+                BaseImmutableModelCreate(self).validate(self.immutability_rules)
+            else:
+                BaseImmutableModelUpdate(self).validate(self.immutability_rules)
         super(ImmutableModel, self).save(*args, **kwargs)
 
-    def update(self, validated_data, force=False, save_kwargs={}):
-        """
-        Set each attribute on the instance, and then save it.
-        In case of error, accumulate them and after checking all fields
-        raise an ValidationError with all error messages
-        :param validated_data: {{*}}
-        :param force: bool if update need to be forced 
-        :param save_kwargs: kwargs params passed to django save method
-        :raise: ValidationError 
-        """
-        errors = {}
-        for field, value in validated_data.items():
-            try:
-                self.update_attribute(field, value, force=force)
-            except ValidationError as exc:
-                errors[field] = exc.detail
-        if errors:
-            raise ValidationError(errors)
-        self.save(**save_kwargs)
+    # def update(self, validated_data, force=False, save_kwargs={}):
+    #     """
+    #     Set each attribute on the instance, and then save it.
+    #     In case of error, accumulate them and after checking all fields
+    #     raise an ValidationError with all error messages
+    #     :param validated_data: {{*}}
+    #     :param force: bool if update need to be forced
+    #     :param save_kwargs: kwargs params passed to django save method
+    #     :raise: ValidationError
+    #     """
+    #     errors = {}
+    #     for field, value in validated_data.items():
+    #         try:
+    #             self.update_attribute(field, value, force=force)
+    #         except ValidationError as exc:
+    #             errors[field] = exc.detail
+    #     if errors:
+    #         raise ValidationError(errors)
+    #     self.save(**save_kwargs)
 
-    def update_attribute(self, name, value, force=False):
-        """
-        Update attribute value only after checking its mutability
-        To force update set parameter force to True 
-        :param name: attribute name
-        :param value: new attribute value
-        """
-        if not force:
-            BaseImmutableModelUpdate(self, name).validate(self.immutability_rules)
-        setattr(self, name, value)
+    # def update_attribute(self, name, value, force=False):
+    #     """
+    #     Update attribute value only after checking its mutability
+    #     To force update set parameter force to True
+    #     :param name: attribute name
+    #     :param value: new attribute value
+    #     """
+    #     if not force:
+    #         BaseImmutableModelUpdate(self, name).validate(self.immutability_rules)
+    #     setattr(self, name, value)
 
     def delete(self, *args, **kwargs):
         """
         Delete object if there is no restrictions
         To force delete set param force to True
         """
-        is_forced_action = kwargs.pop('force', False)
-        if not is_forced_action:
+        tx_force = kwargs.pop('force', False)
+        if not tx_force:
             BaseImmutableModelDelete(self).validate(self.immutability_rules)
         super(ImmutableModel, self).delete(*args, **kwargs)
+
+    def saved_value(self, field):
+        """
+        Mehtod to get field value saved at DB.
+        """
+        raise NotImplementedError('subclasses of ImmutableModel must provide a saved_value() method')
--- tximmutability/rule.py
+++ tximmutability/rule.py
@@ -1,59 +1,70 @@
 # coding=utf-8
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import logging
 
 from django.db.models import FieldDoesNotExist
-from django.db.models.fields.related import ForeignObjectRel, RelatedField
+from django.db.models.fields.related import ForeignObjectRel
+from django.db.models.fields.related import RelatedField
+from django.utils.translation import gettext as _
+
+logger = logging.getLogger('tximmutability')
 
 
-class ImmutabilityRule(object):
+class MutabilityRule:
     """
-    This class serves to define the rule when an model is immutable.
-    To define immutability rule it is obligatory to define field name
-    of the model from which depend model immutability. To define relation field use '__' as separator
+        This class serves to define the rule when an model is immutable.
+        To define immutability rule it is obligatory to define field name
+        of the model from which depend model immutability. To define relation field use '__' as separator
 
-    Once field value is defined model becomes immutable(you can not update it or delete it).
-    To allow update/delete of the object set param allow_update/allow_delete to True.
-    By default immutable object can be created. To forbid creation set allow_create to False
+        Once field value is defined model becomes immutable(you can not update it or delete it).
+        To allow update/delete of the object set param exclude_on_update/exclude_on_delete to True.
+        By default immutable object can be created. To forbid creation set allow_create to False
 
-    To allow object mutability for the specific field's values
-    define that values in mutable_values param
+        To allow object mutability for the specific field's values
+        define that values in values param
 
-    To exclude some fields from the rule and make them mutable define field names in 
-    exclude_fields param
+        To exclude some fields from the rule and make them mutable define field names in
+        exclude_fields param
 
-    Examples:
-        - Only invoice note can be changed if invoice is not in draft state.
-        ImmutabilityRule('estado', mutable_values=('draft',), exclude_fields=('note',))
-        - Entry can not be deleted (but can be updated) if related invoice is validated 
-        ImmutabilityRule('factura__estado', mutable_values=('draft',), allow_update=True)
-        - Invoice line cannot be updated or deleted nor new line can be added if invoice is not in draft or budget state
-        ImmutabilityRule('factura__estado', mutable_values=('draft', 'budget'), allow_create=False)
-    """
-    def __init__(self, field_name, mutable_values=(), exclude_fields=(),
-                 allow_update=False, allow_delete=False, allow_create=True, error_message="", callback=None):
+        Examples:
+            - Only invoice note can be changed if invoice is not in draft state.
+            ImmutabilityRule('estado', values=('draft',), exclude_fields=('note',))
+            - Entry can not be deleted (but can be updated) if related invoice is validated
+            ImmutabilityRule('factura__estado', values=('draft',), allow_update=True)
+            - Invoice line cannot be updated or deleted nor new line can be added if invoice is not in draft or budget state
+            ImmutabilityRule('factura__estado', values=('draft', 'budget'), allow_create=False)
+        """
+
+    def __init__(self, field_name, values=(), exclude_fields=(), exclude_on_create=True, exclude_on_update=False,
+                 exclude_on_delete=False, error_message="", callback=None):
+        """
+        param fields_data: dict {<field_name>: set(<values for each field>)}, like {"name": ('tim', 'tom', 'tam')}
+        """
         self.field_name = field_name
-        self.mutable_values = mutable_values
+        self.values = values
         self.exclude_fields = exclude_fields
 
-        self.allow_update = allow_update
-        self.allow_delete = allow_delete
-        self.allow_create = allow_create
+        self.exclude_on_create = exclude_on_create
+        self.exclude_on_update = exclude_on_update
+        self.exclude_on_delete = exclude_on_delete
 
         self.error_message = error_message
         self.callback = callback
 
-    def excluded_field(self, field_name):
-        field_name in self.exclude_fields
+    def __name__(self):
+        return f"{self.field_name} {self.values}"
 
-    def is_object_mutable(self, model_instance, field_parts=None):
+    def is_mutable(self, model_instance, field_parts=None) -> bool:
         """
         Check if model obj is in mutable state.
-        Model obj is in mutable state if field defined by rule has 
-        one of the values defined in mutable_values. If field is relation check
+        Model obj is in mutable state if field defined by rule has
+        one of the values defined in values. If field is relation check
         if relation is mutable
         :param model_instance: TxerpadBase
-        :param field_parts: name of the field or related object field 
-        (e.g 'estado' or 'factura__estado') 
+        :param field_parts: name of the field or related object field
+        (e.g 'estado' or 'factura__estado')
         :return: bool
         """
         field_parts = field_parts or self.field_name.split('__')
@@ -64,6 +75,7 @@ class ImmutabilityRule(object):
             try:
                 rel = opts.get_field(field_name)
             except FieldDoesNotExist:
+                logger.warning(_("Field does not exist"))
                 return True
             if isinstance(rel, (RelatedField, ForeignObjectRel)):
                 # field is forward or reverse relation
@@ -71,18 +83,18 @@ class ImmutabilityRule(object):
                 if isinstance(rel, ForeignObjectRel):
                     field_name = rel.get_accessor_name()
                 field_val = getattr(model_instance, field_name)
-                return self.is_relation_mutable(rel, field_val, rel_parts)
+                return self._is_mutable_relation(rel, field_val, rel_parts)
             else:
                 # field is model attribute
-                field_val = getattr(model_instance, field_name, None)
-                return field_val in self.mutable_values
-        
+                field_val = model_instance.saved_value(field_name)
+                # field_val = getattr(model_instance, field_name, None)
+                return field_val in self.values
 
-    def is_relation_mutable(self, relation, value, rel_parts):
+    def _is_mutable_relation(self, relation, value, rel_parts):
         """
         Relation is mutable if related object(s) has mutable state.
         If related object is not defined relation is mutable by default.
-        If there are more related objects (relation is many_to_many or one_to_many) 
+        If there are more related objects (relation is many_to_many or one_to_many)
         relation is mutable only if all related objects are mutable
         :param relation: ForeignObjectRel|RelatedField
         :param value: related object
@@ -90,13 +102,13 @@ class ImmutabilityRule(object):
         :return: bool
         """
         if not rel_parts:
-            return value in self.mutable_values
+            return value in self.values
         if not value:
             return True
         if relation.many_to_many or relation.one_to_many:
             for related_object in value.all():
-                if not self.is_object_mutable(related_object, field_parts=rel_parts):
+                if not self.is_mutable(related_object, field_parts=rel_parts):
                     return False
             return True
         else:
-            return self.is_object_mutable(value, field_parts=rel_parts)
+            return self.is_mutable(value, field_parts=rel_parts)
